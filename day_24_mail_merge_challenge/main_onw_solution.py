d936a6815b2596cee4f476978d2423c80195d0d0
RafaelMenchaca <arafael.menchaca@gmail.com>
2025-04-27 22:51:02 -0500

    feat: refactor the code, comments cleared and keep them simple to read

--- day_24_mail_merge_challenge/main_onw_solution.py
+++ day_24_mail_merge_challenge/main_onw_solution.py
@@ -3,33 +3,35 @@
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
 
-#Here each name of invited_names and put in a list named "names"
-with open("./Input/Names/invited_names.txt") as invited_names: #open the file to read it
-    names = invited_names.readlines() # the .readlines() put each line of the file on a list "names"
-print(f"names with \\n: {names}") #print to see how the list working
 
-# new list to save each name as a each item with out the "\n" because that makes a line break on the letter
-names_wout_slash_n = []  # new list empty
-for i in range(len(names)): # this cylce is about ti iterate the number of the total items on the list "names"
-    name_wosn = names[i].replace("\n", "") #this is a variable where we store each name wout "\n" every iterate
-    names_wout_slash_n.append(name_wosn) #here we append each name wout "\n" in to the new empty list
-print(f"names wout \\n: {names_wout_slash_n}") #print to see how the list goes
+# Step 1: Read names from the invited_names.txt file
+with open("./Input/Names/invited_names.txt") as invited_names_file:
+    names = invited_names_file.readlines()  # .readlines() reads each line into a list item
 
-#this for loop itarate the number of total items(names) on the list  names_wout_slash_n
-for i in range(len(names_wout_slash_n)):
-    with open("./Input/Letters/starting_letter.txt") as starting_letter: #here open and read the startin letter (base letter)
-        letter = starting_letter.read()
-    new_letter = letter.replace("[name]", names_wout_slash_n[i]) #here we are replacing each name on each letter
-    #new_letter save on each iteration a new letter with different name and then each letter
-    #with each name is aout to create a new file bc doesn't exist already, and if is exist
-    #is about to rewrite with the name used
-    with open(f"./Output/ReadyToSend/{names_wout_slash_n[i]}", mode="w") as filetxt: #here we create or modify the file wth each name iterated
-        letter = filetxt.write(new_letter) #here we write the letter
-        #the letter is the new letter with the name replaced
-        #and the file is the file created or modified with the name used
-        #the mode "w" is to write the file, if the file exist it will be rewrite
-        #if the file doesn't exist it will be created
-        #the file is the file created or modified with the name used
+print(f"Names with '\\n': {names}")  # Debug print: shows the raw names list including newline characters
+
+# Step 2: Clean the names (remove the '\n') and save them into a new list
+names_without_newline = []  # Empty list to store clean names
+
+for i in range(len(names)):  
+    name_clean = names[i].replace("\n", "")  # Remove newline character from each name
+    names_without_newline.append(name_clean)  # Add the clean name to the new list
+
+print(f"Names without '\\n': {names_without_newline}")  # Debug print: shows clean names list
+
+# Step 3: Create and save a personalized letter for each name
+for i in range(len(names_without_newline)):
+    # Open and read the starting letter template
+    with open("./Input/Letters/starting_letter.txt") as starting_letter_file:
+        letter_template = starting_letter_file.read()
+
+    # Replace the placeholder [name] with the actual name
+    personalized_letter = letter_template.replace("[name]", names_without_newline[i])
+
+    # Save the personalized letter into a new file named after the invitee
+    with open(f"./Output/ReadyToSend/{names_without_newline[i]}", mode="w") as output_file:
+        output_file.write(personalized_letter)
+
 
     
 
