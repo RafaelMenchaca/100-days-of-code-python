6a5a54fb3c79098ab9632b55a77336b44770a66b
RafaelMenchaca <arafael.menchaca@gmail.com>
2025-05-05 20:30:01 -0500

    label, input, and button intro TKinter

--- day_27_tkinter_intro/main.py
+++ day_27_tkinter_intro/main.py
@@ -1,16 +1,36 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
 
 #Label
+my_label = Label(text="I am a label", font=("Arial", 24))
+my_label.pack()
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
-my_label.pack(side="left")
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
+#Button
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+#Entry
+
+input = Entry(width=10)
+input.pack()
+print(input.get())
 
 
 
