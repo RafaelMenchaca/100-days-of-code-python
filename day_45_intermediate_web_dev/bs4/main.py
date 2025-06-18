a22bbbe7ca49e45b5730b27d0de612846184b17a
RafaelMenchaca <arafael.menchaca@gmail.com>
2025-06-17 21:49:17 -0500

    Day 45 completed web scraping with python

--- day_45_intermediate_web_dev/bs4/main.py
+++ day_45_intermediate_web_dev/bs4/main.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup
-import lxml
 import requests
 
-response = requests.get("https://news.ycombinator.com/news")
+response = requests.get("https://appbrewery.github.io/news.ycombinator.com/")
+
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-article_titles = soup.find_all(name="span", class_="titleline")
-print(article_titles)
-                               
+# print(soup.title)
 
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+    
+article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
+# print(article_texts)
+# print(article_links)
+# print(article_upvote)
+# print(article_texts[0])
+# print(article_links[0])
+# print(article_upvote[0])
 
+largest_number = max(article_upvote)
+largest_index = article_upvote.index(largest_number)
+print(article_texts[largest_index])
+print(article_links[largest_index])
 
 
 
