a9f9ecd0774268ff3a6e97129705a4d10211adee
RafaelMenchaca <arafael.menchaca@gmail.com>
2025-07-03 23:29:23 -0500

    Day 53 startet, just imports

---
+++ day_53_data_entry_job/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from dotnet import load_dotenv
+from beautifulsoup4 import BeautifulSoup
+
+load_dotenv()
+# Credentials and constants
