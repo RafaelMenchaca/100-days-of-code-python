459c8696141f922c66ed971968dfd1c219aa7c46
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-08-14 20:21:51 -0500

    day 62 in progress, flask, bootstrap, jinja2, csv files, created and modified

---
+++ day_62_flask_wtforms_bootstrap/day_62/main2.py
@@ -0,0 +1,16 @@
+import csv
+
+with open('day_62_flask_wtforms_bootstrap\day_62\cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+    csv_data = csv.reader(csv_file, delimiter=',')
+    list_of_rows = []
+    for row in csv_data:
+        list_of_rows.append(row)
+    cafes = list_of_rows
+
+# print(cafes)
+# print(cafes)
+
+print(cafes[0])
+
+for cafe in cafes:
+    print(cafe[0])  # Print the name of each cafe
