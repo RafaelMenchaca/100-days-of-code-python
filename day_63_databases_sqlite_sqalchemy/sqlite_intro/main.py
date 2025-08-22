a7d6013a4df0b72beef1f2c3764eeb1026f746cf
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-08-21 20:17:54 -0500

    day 63 in progress SQLAlchemy intro

--- day_63_databases_sqlite_sqalchemy/sqlite_intro/main.py
+++ day_63_databases_sqlite_sqalchemy/sqlite_intro/main.py
@@ -1,11 +1,28 @@
-import sqlite3
+# import sqlite3
 
-db = sqlite3.connect("books-collection.db")
+# db = sqlite3.connect("books-collection.db")
 
-cursor = db.cursor()
+# cursor = db.cursor()
 
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) " \
-# "NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) " \
+# # "NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class Base(DeclarativeBase):
+    pass
+class Book(Base):
+    __tablename__ = 'books'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+
