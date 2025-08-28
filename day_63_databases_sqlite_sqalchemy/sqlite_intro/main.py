2cc6a431a1416d74749c635ecabcabde9ea00267
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-08-27 21:24:46 -0500

    day 63 in progress, satabase .db conected, now i can post to the db and get what is on it, SQLAlquemy db.model

--- day_63_databases_sqlite_sqalchemy/sqlite_intro/main.py
+++ day_63_databases_sqlite_sqalchemy/sqlite_intro/main.py
@@ -1,28 +1,52 @@
-# import sqlite3
-
-# db = sqlite3.connect("books-collection.db")
-
-# cursor = db.cursor()
-
-# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) " \
-# # "NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-
-# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-# db.commit()
-
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
+import os
+
+
+DB_FILE_PATH = os.path.join(
+    os.path.dirname(__file__),
+    'books-collection.db')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_FILE_PATH}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
 class Base(DeclarativeBase):
     pass
-class Book(Base):
-    __tablename__ = 'books'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
 
+class Book(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    title = db.Column(String(250), unique=True, nullable=False)
+    author = db.Column(String(250), nullable=False)
+    rating = db.Column(Float, nullable=False)
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", all_books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        title = request.form.get("book_title")
+        author = request.form.get("book_author")
+        rating = float(request.form.get("book_rating"))
+        
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
