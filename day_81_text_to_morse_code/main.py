95333b660d5c2a3891d0f2e39836085acef784fc
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-10-08 20:28:05 -0500

    Day 82 completed, Professional Project: Python Scripting

---
+++ day_81_text_to_morse_code/main.py
@@ -0,0 +1,49 @@
+text_from_user = input("Write your message to convert to Morse Code: \n").upper()
+
+morse_code_dict = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    " ": "/"
+}
+
+morse_code = ""
+
+#loop on each letter to convert it
+
+for letters in text_from_user:
+    morse_code += morse_code_dict[letters] + " "
+print(morse_code)
